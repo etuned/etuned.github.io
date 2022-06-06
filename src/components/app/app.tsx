1e0cfd08ce90d155d51c00fd1695c56ad3ab423d
Edwin Bartunek <edwin@devhart.io>
2022-06-06 18:47:31 +0900

    Quik UseStyles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,9 @@
-import { useStore, component$, Host } from '@builder.io/qwik';
+import { useStore, component$, Host, useStyles$ } from '@builder.io/qwik';
 import { Logo } from '../logo/logo';
+import GlobalCSS from './global.css'
 
 export const App = component$(() => {
+  useStyles$(GlobalCSS);
   const state = useStore({ name: 'World' });
   return (
     <Host>
