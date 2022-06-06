40891f3f7972a4653fe8c84efc38064bf90ea47c
Edwin Bartunek <edwin@devhart.io>
2022-06-06 18:09:44 +0900

    removed template content

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -3,13 +3,8 @@ import { component$, Host } from '@builder.io/qwik';
 export const Logo = component$(() => {
   return (
     <Host style={{ 'text-align': 'center' }}>
-      <a href="https://qwik.builder.io/">
-        <img
-          alt="Qwik Logo"
-          width={400}
-          height={147}
-          src="https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F667ab6c2283d4c4d878fb9083aacc10f"
-        />
+      <a href="https://semishigure.cloud/">
+        <h1>Semishigure.cloud</h1>
       </a>
     </Host>
   );
