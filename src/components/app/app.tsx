531ceb3f6715cabea4349d282a3d5aaeac8ffbca
Edwin Bartunek <edwin@devhart.io>
2022-06-06 18:41:31 +0900

    global css location adjustment

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,11 @@
 import { useStore, component$, Host } from '@builder.io/qwik';
 import { Logo } from '../logo/logo';
 
-import './global.css';
-
 export const App = component$(() => {
   const state = useStore({ name: 'World' });
   return (
-    <Host class="my-app p-20">
-      <Logo class="mb-10" />
+    <Host>
+      <Logo />
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '35rem' }}>
         <h1 class="text-3xl mb-2">Hi!</h1>
       </div>
